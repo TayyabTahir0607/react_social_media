ae8f407c9f44b5215f52caf7b4bdf8319c287cad
TayyabTahir0607 <tayyabtahir0607@gmailcom>
2024-10-03 23:12:03 +0500

    initilised react routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreatePost from "./components/CreatePost.jsx";
 
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/create-post", element: <CreatePost /> },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
